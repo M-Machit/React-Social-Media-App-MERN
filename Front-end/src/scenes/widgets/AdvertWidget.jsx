c67bd67def6f736010e040222000bb54748e1198
unknown <simomachit@gmail.com>
2023-05-28 19:14:32 +0200

    Initial commit

---
+++ Front-end/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,39 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+    const { palette } = useTheme();
+    const dark  = palette.neutral.dark;
+    const main = palette.neutral.main;
+    const medium = palette.neutral.main;
+
+    return (
+        <WidgetWrapper >
+            <FlexBetween>
+                <Typography color={dark} variant="h5" fontWeight="500"> 
+                    sponsored
+                </Typography>
+                <Typography color={medium}> Create Ad </Typography>
+            </FlexBetween>
+            <img
+             width="100%"
+             height="auto"
+             alt="advert"
+             src="http://localhost:3001/assets/advert.png"
+             style={{ borderRadius : "0.75rem", margin : "0.75rem 0"}}
+
+            />
+            <FlexBetween>
+                <Typography color={main} > Machit Web </Typography> 
+                <Typography color={medium} > machitweb.com </Typography>
+            </FlexBetween>
+            <Typography color={medium} m="0.5rem 0" >
+                your pathway to creat your owen Web-site
+            </Typography>
+        </WidgetWrapper>
+    );
+
+};
+
+export default AdvertWidget ;
